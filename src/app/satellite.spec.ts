7df30c78eaf30633ef251eb01acd8899c1a76205
Meredith Schwartz <MSchwartz@office.clubready.local>
2020-08-03 20:48:00 -0500

    added code

--- src/app/satellite.spec.ts
+++ src/app/satellite.spec.ts
@@ -2,6 +2,6 @@ import { Satellite } from './satellite';
 
 describe('Satellite', () => {
   it('should create an instance', () => {
-    expect(new Satellite()).toBeTruthy();
+    expect(new Satellite("sample", "sample", "sample", "sample", true)).toBeTruthy();
   });
 });
